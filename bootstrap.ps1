1cce02120c0808626c91034272bbd00dea9286ea
Richard Slater <richard.slater@amido.com>
2021-10-19 14:39:41 +0100

    Add `ll` Alias and tidy up of `bootstrap.ps1`

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -2,7 +2,25 @@
 
 Set-ExecutionPolicy Bypass -Scope Process -Force;
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
-iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'));
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'));
 
 Set-Service -Name "ssh-agent" -StartupType Automatic
 Start-Service -Name "ssh-agent"
+
+$repoProfile = Join-Path -Path $PSScriptRoot -ChildPath "Microsoft.PowerShell_profile.ps1"
+if (Test-Path $repoProfile) {
+    if (Test-Path $PROFILE) {
+        Remove-Item $PROFILE;
+    }
+    New-Item -Path $PROFILE -ItemType SymbolicLink -Value $repoProfile;
+}
+
+$repoScriptsPath = Join-Path -Path $PSScriptRoot -ChildPath "scripts"
+$profilePath = Split-Path $PROFILE
+$scriptsPath = Join-Path $profilePath -ChildPath "scripts"
+if (Test-Path $repoScriptsPath) {
+    if (Test-Path $scriptsPath) {
+        Remove-Item $scriptsPath -Recurse -Force;
+    }
+    New-Item -Path $scriptsPath -ItemType SymbolicLink -Value $repoScriptsPath;
+}
--- scripts/aliases.ps1
+++ scripts/aliases.ps1
@@ -1,2 +1,4 @@
 Function gitviz {git log --oneline --decorate --all --graph --simplify-by-decoration}
 Function gpoh {git push origin head}
+
+Set-Alias -Name ll -Value Get-ChildItemColor
